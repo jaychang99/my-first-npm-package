33d3e847f4706558296e5a56f404f49e7c92a4c2
jaychang99 <jaychang99@gmail.com>
2024-04-06 08:33:44 +0900

    feat: add utility functions

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-exports.printMsg = function () {
+export function printMsg() {
   console.log("This is a message from the demo package. Made by jaychang99");
+}
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
+
+export function multiply(a, b) {
+  return a * b;
+}
+
+export function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Cannot divide by zero");
+  }
+  return a / b;
 }
